612f3202502328707451a6ba7463b4efb6326ed7
54222 <54222@THIAGO>
2023-05-09 18:53:49 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio20.consola/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Ingrese el primer numero");
+var numeroUno=int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo numero");
+var numeroDos=int.Parse(Console.ReadLine());
+if (numeroUno==numeroDos)
+{
+    Console.WriteLine("Ambos son iguales");
+
+}
+else if (numeroUno > numeroDos)
+{
+    Console.WriteLine($"{numeroUno} es mayor a {numeroDos}");
+}
+else
+{
+    Console.WriteLine($"{numeroDos} es mayor a {numeroUno}");
+}
